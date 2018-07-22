3463e133e176fd3be09d73c8223226b90d72a395
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2018-07-22 23:04:50 +0700

    Changes to InputForm and OverviewPanel class

--- src/input/InputForm.java
+++ src/input/InputForm.java
@@ -17,7 +17,6 @@ import datadriver.DataDriver;
 import giantsweetroll.gui.swing.ScrollPaneManager;
 import giantsweetroll.message.MessageManager;
 import gui.MainFrame;
-import gui.OverviewPanel;
 import gui.methods.FileOperation;
 import interfaces.GUIMethods;
 
@@ -36,16 +35,14 @@ public abstract class InputForm extends JPanel implements ActionListener, GUIMet
 	private String prevPanelName, nextPanelName;
 	private JScrollPane scroll;
 	private boolean newEntry;
-	private OverviewPanel overviewPanel;
 	
 	//Constants
 	private final String CANCEL = "cancel";
 	private final String SAVE = "save";
 	
 	//Constructor
-	public InputForm(String formName, String prevPanelName, String nextPanelName, OverviewPanel overviewPanel)
+	public InputForm(String formName, String prevPanelName, String nextPanelName)
 	{
-		this.overviewPanel = overviewPanel;
 		this.init(formName, prevPanelName, nextPanelName);
 	}
 	//Create GUI
@@ -119,7 +116,6 @@ public abstract class InputForm extends JPanel implements ActionListener, GUIMet
 	//Abstract methods
 	public abstract DataDriver getData();
 	public abstract void setData(DataDriver data);
-	public abstract void refreshTable();
 	
 	//Interfaces
 	@Override
